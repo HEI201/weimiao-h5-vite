ff0ec67ad9c2b8c3fa5b614b8372efbe4e18800a
jackie <lianghengwei2011@163.com>
2023-12-03 18:20:37 +0800

    Add navigation to courses page

--- src/app.jsx
+++ src/app.jsx
@@ -1,8 +1,13 @@
-import { Outlet } from 'react-router-dom';
+import { useEffect } from 'react';
+import { Outlet, useNavigate } from 'react-router-dom';
 import { CourseProvider } from './context';
 import { MyBreadcrumbs } from './my-breadcrumbs';
 
 export default function App() {
+  const navigate = useNavigate()
+  useEffect(() => {
+    navigate('/courses')
+  }, [])
   return (
     <CourseProvider>
       <MyBreadcrumbs></MyBreadcrumbs>
